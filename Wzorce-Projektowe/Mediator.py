9c9e31f2181c0f889ee8d4bd05cdb031ebf73b81
julia741 <73189357+julia741@users.noreply.github.com>
2022-06-16 18:38:56 +0200

    Create Mediator.py

---
+++ Wzorce-Projektowe/Mediator.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+class User(object):
+    def __init__(self, name, mediator):
+        self.name = name
+        self.mediator = mediator
+
+    def send(self, message):
+        mediator.ping(self.name, message)
+
+
+class Mediator(object):
+    def ping(self, name, message):
+        print("Użytkownik {}: {}".format(name, message))
+
+if __name__ == "__main__":
+    mediator = Mediator()
+
+    basia = User("Basia", mediator)
+    kasia = User("Kasia", mediator)
+
+    basia.send("Hej, co tam?")
+    kasia.send("A dobrze, a u Ciebie?")
